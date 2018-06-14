0dc9ff49f9c30fee76d943bfdb5495090c55a506
Piansis <kritteewut@users.noreply.github.com>
2018-06-14 11:11:17 +0700

    Initial commit

---
+++ src/Components/config.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import firebase from 'firebase';
+config = {
+    apiKey: "AIzaSyA57DUcNR8v9ru5z-hHQgQbVLJmDBAF-G4",
+    authDomain: "test-project-ling.firebaseapp.com",
+    databaseURL: "https://test-project-ling.firebaseio.com",
+    projectId: "test-project-ling",
+    storageBucket: "test-project-ling.appspot.com",
+    messagingSenderId: "989521686632"
+  };
+  firebase.initializeApp(config);
